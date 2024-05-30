3893ae27b07423f30e2292354294d7ac4df3ec48
dkMaster3000 <kunzd007@gmail.com>
2024-05-30 12:30:00 +0200

    reading excel file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,56 @@
 package org.example;
 
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream file = new FileInputStream(new File("C:\\Users\\kunzd\\IdeaProjects\\PnPCharacterCreator\\src\\main\\resources\\PnPCharacterCreator.xlsx"));
+        Workbook workbook = new XSSFWorkbook(file);
+
+        Sheet sheet = workbook.getSheet("Rassen");
+
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Row row : sheet) {
+            data.put(i, new ArrayList<String>());
+            for (Cell cell : row) {
+
+                switch (cell.getCellType()) {
+                    case STRING:
+                        data.get(i).add(cell.getRichStringCellValue().getString());
+                        break;
+                    case NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            data.get(i).add(cell.getDateCellValue() + "");
+                        } else {
+                            data.get(i).add(cell.getNumericCellValue() + "");
+                        }
+                        break;
+                    case BOOLEAN:
+                        data.get(i).add(cell.getBooleanCellValue() + "");
+                        break;
+                    case FORMULA:
+                        data.get(i).add(cell.getCellFormula() + "");
+                        break;
+                    default:
+                        data.get(i).add(" ");
+                }
+
+            }
+            i++;
+        }
+
+        System.out.println(data);
     }
 }
